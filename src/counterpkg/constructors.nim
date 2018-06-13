9ac340173201abdea187828fd3b44530e867a401
CodeDoes <codedoes@gmail.com>
2018-06-13 17:12:49 +0200

    moved to nimble package folder layout

--- src/counterpkg/constructors.nim
+++ src/counterpkg/constructors.nim
@@ -1,15 +1,8 @@
 import nimgame2 / [entity]
 
-###  unsafe constructor example  ###
-# proc new(a:out Entity)=
-#   a=newEntity()
-###  example end  ###
 proc new*[](T:type Entity,a:out T)=
   a=newEntity()
 
-# proc new[](T:typedesc[Entity]):T=Entity.new result
-# proc new*[](T:typedesc[int],a:out T)=
-#   a=T()
 proc new[](_:type int,value:int):ref int=
   new result
   result[]=value
